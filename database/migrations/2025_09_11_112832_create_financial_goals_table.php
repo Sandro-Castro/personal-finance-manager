eff30a338959571cce8c5d61ffd2821e72a536ea
Sandro-Castro <sandro.oliveira5549@gmail.com>
2025-09-11 08:30:55 -0300

    add migration for financial goals

---
+++ database/migrations/2025_09_11_112832_create_financial_goals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_goals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('target_amount', 10, 2);
+            $table->decimal('current_amount', 10, 2)->default(0);
+            $table->date('deadline');
+            $table->string('status')->default('in_progress'); 
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_goals');
+    }
+};
